a843cb108f605e8f0be42890260471c5c477d5a9
Dave Bowker <dave@davebowker.com>
2013-12-15 21:40:14 +0000

    Pointing index.php to the correct location

--- index.php
+++ index.php
@@ -14,4 +14,5 @@
 define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+require('./wordpress/wp-blog-header.php');
+
