2d4d80985020909bcbf77d045ade692d3f24aa54
davebowker <dave@davebowker.com>
2013-12-18 15:34:16 +0000

    added wp to gitignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,39 @@
 module.exports = function(grunt) {
 	
+	// Set UTF-8 Charset
+	grunt.file.defaultEncoding = 'utf8';
+	
+	// Set our instructions
 	grunt.initConfig({
 		// Read package.json for instructions on what packages to use
 		pkg: grunt.file.readJSON('package.json'),
-		copy: {
-			moveWordPress: {
-				src: 'wordpress/**',
-				dest: 'app/',
-				expand: true,
-				cwd: 'bower_components'
-			}
-		},
+		// Copy WordPress into the app folder
+		copy: [
+				// Copy WordPress
+				{
+					expand: true,
+					cwd: 'bower_components',
+					src: 'wordpress/**',
+					dest: 'app/'
+				},
+				// Copy wp-content
+				{
+					expand: true,
+					cwd: 'bower_components/wordpress',
+					src: 'wp-content/**',
+					dest: 'app'
+				}
+				// includes files within path and its sub-directories
+				//{expand: true, src: ['path/**'], dest: 'dest/'},
+				
+				// makes all src relative to cwd
+				//{expand: true, cwd: 'path/', src: ['**'], dest: 'dest/'},
+				
+				// flattens results to a single level
+				//{expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'}
+			
+		],
+		// Remove bower copy of wordpress
 		clean: {
 			deleteBowerWordPress: {
 				src: 'bower_components/wordpress'
@@ -23,9 +46,20 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	
 	// Define grunt tasks
-	grunt.registerTask('setupWordPress', ['copy']);
+	grunt.registerTask ('setupWordPress', function() {
+		grunt.file.write("app/index.php", '<?php define(\'WP_USE_THEMES\',true);require(dirname(__FILE__).\'/wordpress/wp-blog-header.php\');');
+		grunt.task.run('copy');
+	});	
+	//grunt.registerTask('setupWordPress', ['copy']);
 	
-	// We want to disable default grunt for now
-	//grunt.registerTask('default', []);
+	
+	// We want to disable default grunt for now, so provide a nice message
+	grunt.registerTask('default', 'My "default" task description.', function() {
+		grunt.log.writeln('');
+		grunt.log.writeln('********************************************');
+		grunt.log.writeln('There are no default tasks in this project.');
+		grunt.log.writeln('Open Gruntfile.js to see how Grunt is built.');
+		grunt.log.writeln('********************************************');
+	});
 
 };
