e5a19dc485c38661b3fb1c0deb9fadcb07ab9c54
davebowker <dave@davebowker.com>
2014-01-02 14:35:56 +0000

    updated gruntfile to remove wpconfig sample

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function(grunt) {
 		},
 		clean : {
 			// WordPress
-			wp : ['app/wp-content/plugins/hello.php', 'app/wp-content/themes/twentyten', 'app/wp-content/themes/twentyeleven', 'app/wp-content/themes/twentytwelve']
+			wp : ['app/wordpress/wp-config-sample.php', 'app/wp-content/plugins/hello.php', 'app/wp-content/themes/twentyten', 'app/wp-content/themes/twentyeleven', 'app/wp-content/themes/twentytwelve']
 		},
 		compass : {
 			dev : {
