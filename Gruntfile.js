9966eb603947cee2ef29965378fdeca1e037ce2e
davebowker <dave@davebowker.com>
2014-01-02 14:13:38 +0000

    updating jsons

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,6 +59,28 @@ module.exports = function(grunt) {
 					force : true
 				}
 			}
+		},
+		replace: {
+			wpconfig: {
+				src: ['app/wp-config.php'],
+				overwrite: true,
+				//dest: 'app/wp-config.php',
+				replacements: [{
+					from: '<?php',
+					to: function (matchedWord) {   // callback replacement
+						//return matchedWord;
+						var phpTag = matchedWord + '\n\n\n';
+						var prewrap1 = "/***** Dynamicaly generated from Gruntfile.js *****/ \n"
+						var prewrap2 = "/***** You may need to update the path to your project *****/ \n"
+					    var wp_home = "define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME'] . '/eikoncodex.com/app'); \n"; 
+					    var wp_siteurl = "define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/eikoncodex.com/app/wordpress'); \n";
+					    var wp_content_dir = "define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/eikoncodex.com/app/wp-content'); \n";
+					    var wp_content_url = "define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/eikoncodex.com/app/wp-content'); \n";
+						var postwrap = "/***** End Gruntfile.js inserts *****/ \n\n\n"	    
+					    return [phpTag + prewrap1 + prewrap2 + wp_home + wp_siteurl + wp_content_dir + wp_content_url + postwrap];
+					}
+				}]
+			}
 		}
 	});
 
@@ -66,13 +88,18 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-text-replace');
 
 	// Define grunt tasks
 	grunt.registerTask('wp', function() {
 		grunt.file.write("app/index.php", '<?php define(\'WP_USE_THEMES\',true);require(dirname(__FILE__).\'/wordpress/wp-blog-header.php\');');
-		grunt.task.run('copy:wp');
+		grunt.task.run('copy:wp', 'clean:wp');
 	});
 	grunt.registerTask('wpsass', 'compass');
+	grunt.registerTask('wpconfig', function() {
+		grunt.task.run('replace:wpconfig');
+		grunt.log.write('Guts has dynamically added new lines to the top of your wp-config.php file in the app folder. You will need to edit these lines. Please do this now./');
+	});
 
 	// We want to disable default grunt for now, so provide a nice message
 	grunt.registerTask('default', 'My "default" task description.', function() {
