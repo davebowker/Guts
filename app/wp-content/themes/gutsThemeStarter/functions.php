e6e0b780820a8de9491ed0fdb103a365a3611e44
davebowker <dave@davebowker.com>
2014-01-13 15:17:45 +0000

    changed

--- app/wp-content/themes/gutsThemeStarter/functions.php
+++ app/wp-content/themes/gutsThemeStarter/functions.php
@@ -13,3 +13,18 @@ require_once( get_stylesheet_directory() . '/inc/admin.php' );
  */
 
 //require_once( get_template_directory() . '/lib/class-tgm-plugin-activation.php' );
+
+add_filter( 'template_include', 'var_template_include', 1000 );
+function var_template_include( $t ){
+     $GLOBALS['current_theme_template'] = basename($t);
+     return $t;
+}
+
+function get_current_template( $echo = false ) {
+     if( !isset( $GLOBALS['current_theme_template'] ) )
+         return false;
+     if( $echo )
+         echo $GLOBALS['current_theme_template'];
+     else
+         return $GLOBALS['current_theme_template'];
+}
