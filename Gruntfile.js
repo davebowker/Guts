a6dc811b0ce254ec0ad636ea61d0e8d44f7fae4c
davebowker <dave@davebowker.com>
2014-01-13 16:37:15 +0000

    grunt changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -124,18 +124,18 @@ module.exports = function(grunt) {
 					to: function (matchedWord) {   // callback replacement
 						//return matchedWord;
 						var phpTag = matchedWord + '\n\n\n';
-						var prewrap1 = "/***** Dynamicaly generated from Gruntfile.js *****/ \n"
-						var prewrap2 = "/***** You may need to update the path to your project *****/ \n"
+						var prewrap1 = "/***** Dynamicaly generated from Gruntfile.js *****/ \n";
+						var prewrap2 = "/***** You may need to update the path to your project *****/ \n";
 					    var wp_home = "define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME'] . '/app'); \n"; 
 					    var wp_siteurl = "define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/app/wordpress'); \n";
 					    var wp_content_dir = "define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/app/wp-content'); \n";
 					    var wp_content_url = "define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/app/wp-content'); \n";
-					    var disallowFileEdit = "define('DISALLOW_FILE_EDIT', true);";
-						var compressCSS = "define( 'COMPRESS_CSS', true )";
-						var compressScripts = "define( 'COMPRESS_SCRIPTS', true )";
-						var concatenateScripts = "define( 'CONCATENATE_SCRIPTS', true )";
-						var enforceGZIP = "define( 'ENFORCE_GZIP',        true )";
-						var postwrap = "/***** End Gruntfile.js inserts *****/ \n\n\n"	    
+					    var disallowFileEdit = "define('DISALLOW_FILE_EDIT', true); \n";
+						var compressCSS = "define('COMPRESS_CSS', true); \n";
+						var compressScripts = "define('COMPRESS_SCRIPTS', true); \n";
+						var concatenateScripts = "define('CONCATENATE_SCRIPTS', true); \n";
+						var enforceGZIP = "define('ENFORCE_GZIP', true); \n";
+						var postwrap = "/***** End Gruntfile.js inserts *****/ \n\n\n";
 					    return [phpTag + prewrap1 + prewrap2 + wp_home + wp_siteurl + wp_content_dir + wp_content_url + disallowFileEdit + compressCSS + compressScripts + concatenateScripts + enforceGZIP + postwrap];
 					}
 				}]
