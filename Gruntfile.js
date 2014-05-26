c0ce4f121b3558af833579959efbf1f4e7ab4e73
Dave Bowker <dave@davebowker.com>
2014-05-26 19:05:55 +0100

    Updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,21 @@
 /*
- * TODO: 
+ * TODO:
  * 1) Deploy script to deploy only the app directory
  * 2) Image Compression
  * 3) JS concat
  * 4) CSS + JS Lint
  * 5) Watch for js
- * 6) For deploy, do not delete the wp-config-sample but rather do not upload it to production server
+ * 6) For deploy, do not delete the wp-config-sample but rather do not upload it to production server. MAKE AN AWESOME BUILD FILE.
+ * 7) In the watch task, can we pick up the file names/options from the tasks above?? EG. files: ['<%= jshint.files %>']
+ * 8) grunt contrib server
+ * 9) 
  */
 
 module.exports = function(grunt) {
 
 	// Set UTF-8 Charset
 	grunt.file.defaultEncoding = 'utf8';
-	
+
 	// Load grunt packages
 	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
@@ -41,18 +44,18 @@ module.exports = function(grunt) {
 			setup : ['app/wordpress/wp-config-sample.php', 'app/wp-content/plugins/hello.php', 'app/wp-content/themes/twentyten', 'app/wp-content/themes/twentyeleven', 'app/wp-content/themes/twentytwelve']
 			//wpconfig: ['app/wp-config-sample.php']
 		},
-        autoprefixer: {
-            options: {
-                browsers: ['last 2 versions', '> 1%', 'ie 8', 'ie 9']
-            },
-		    dev: {
-		      options: {
-		        // Target-specific options go here.
-		      },
-		      src: 'app/wp-content/themes/gutsThemeStarter/style.css',
-		      dest: 'app/wp-content/themes/gutsThemeStarter/style.css'
-		    }
-        },
+		autoprefixer : {
+			options : {
+				browsers : ['last 2 versions', '> 1%', 'ie 8', 'ie 9']
+			},
+			dev : {
+				options : {
+					// Target-specific options go here.
+				},
+				src : 'app/wp-content/themes/gutsThemeStarter/style.css',
+				dest : 'app/wp-content/themes/gutsThemeStarter/style.css'
+			}
+		},
 		compass : {
 			dev : {
 				options : {
@@ -88,71 +91,87 @@ module.exports = function(grunt) {
 				}
 			}
 		},
-		/*
-		 * TODO: Compile js files and compress images
-		 */
-		watch: {
-			// Watch our scss files and auto compile them
-			style: {
-				files: [
-					'app/wp-content/themes/gutsThemeStarter/scss/*.scss',
-					'app/wp-content/themes/gutsThemeStarter/scss/**/*.scss'
-				],
-				tasks: ['compass:dev', 'autoprefixer:dev'],
-				options: {
-					livereload: true
-				}
-			},
-			// Watch our js files and auto compile them
-			js: {
-				files: ['app/wp-content/themes/gutsThemeStarter/js/source/*.js'],
-				tasks: ['uglify']
-			},
-			// Watch our files for any changes, then automatically reload the page
-			// Requires livereload chrome extension, or equivalent
-			livereload: {
-				files: [
-					'app/wp-content/themes/gutsThemeStarter/css/**.*',
-					'app/wp-content/themes/gutsThemeStarter/**'
-				],
-				options: {
-					livereload: true
-				}
-			}
-		},
-		replace: {
-			setup: {
-				src: ['app/wp-config-sample.php'],
-				overwrite: true,
+		replace : {
+			setup : {
+				src : ['app/wp-config-sample.php'],
+				overwrite : true,
 				//dest: 'app/wp-config.php',
-				replacements: [{
-					from: '<?php',
-					to: function (matchedWord) {   // callback replacement
+				replacements : [{
+					from : '<?php',
+					to : function(matchedWord) {// callback replacement
 						//return matchedWord;
 						var phpTag = matchedWord + '\n\n\n';
 						var prewrap1 = "/***** Dynamicaly generated from Gruntfile.js *****/ \n";
 						var prewrap2 = "/***** You may need to update the path to your project *****/ \n";
-					    var wp_home = "define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME'] . '/app'); \n"; 
-					    var wp_siteurl = "define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/app/wordpress'); \n";
-					    var wp_content_dir = "define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/app/wp-content'); \n";
-					    var wp_content_url = "define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/app/wp-content'); \n";
-					    var disallowFileEdit = "define('DISALLOW_FILE_EDIT', true); \n";
+						var wp_home = "define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME'] . '/app'); \n";
+						var wp_siteurl = "define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/app/wordpress'); \n";
+						var wp_content_dir = "define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/app/wp-content'); \n";
+						var wp_content_url = "define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/app/wp-content'); \n";
+						var disallowFileEdit = "define('DISALLOW_FILE_EDIT', true); \n";
 						var compressCSS = "define('COMPRESS_CSS', true); \n";
 						var compressScripts = "define('COMPRESS_SCRIPTS', true); \n";
 						var concatenateScripts = "define('CONCATENATE_SCRIPTS', true); \n";
 						var enforceGZIP = "define('ENFORCE_GZIP', true); \n";
 						var postwrap = "/***** End Gruntfile.js inserts *****/ \n\n\n";
-					    return [phpTag + prewrap1 + prewrap2 + wp_home + wp_siteurl + wp_content_dir + wp_content_url + disallowFileEdit + compressCSS + compressScripts + concatenateScripts + enforceGZIP + postwrap];
+						return [phpTag + prewrap1 + prewrap2 + wp_home + wp_siteurl + wp_content_dir + wp_content_url + disallowFileEdit + compressCSS + compressScripts + concatenateScripts + enforceGZIP + postwrap];
 					}
 				}]
+			}
+		},
+		/*
+		 * TODO: Understand this!
+		 */
+		uglify : {
+			options: {
+				mangle: {
+					except: ['jQuery', 'Backbone']
+				},
+				compress: {
+					drop_console: true
+				},
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd hh:mm") %> */',
+				// Enable to beautify the code for debugging
+				beautify: false
+			},
+			dev : {
+				files : {
+					'app/wp-content/themes/gutsThemeStarter/js/script.js' : ['app/wp-content/themes/gutsThemeStarter/js/source/*.js']
+				}
+			}
+		},
+		/*
+		 * TODO: Compile js files and compress images
+		 */
+		watch : {
+			// Watch our scss files and auto compile them
+			style : {
+				files : ['app/wp-content/themes/gutsThemeStarter/scss/*.scss', 'app/wp-content/themes/gutsThemeStarter/scss/**/*.scss'],
+				tasks : ['compass:dev', 'autoprefixer:dev'],
+				options : {
+					livereload : true
+				}
+			},
+			/*
+			 * TODO: This needs a 'files' attribute. Need to load in jshint first.
+			 */
+			js : {
+				tasks : ['uglify:dev']
+			},
+			// Watch our files for any changes, then automatically reload the page
+			// Requires livereload chrome extension, or equivalent
+			livereload : {
+				files : ['app/wp-content/themes/gutsThemeStarter/css/**.*', 'app/wp-content/themes/gutsThemeStarter/**'],
+				options : {
+					livereload : true
+				}
 			}
 		}
 	});
 
 	/*
-	 * Define Grunt Tasks
-	 */
-	
+	* Define Grunt Tasks
+	*/
+
 	// Set up wordpress, copy across wp-content and create index.php and wp-config-sample.php
 	grunt.registerTask('setup', function() {
 		grunt.file.write("app/index.php", '<?php define(\'WP_USE_THEMES\',true);require(dirname(__FILE__).\'/wordpress/wp-blog-header.php\');');
@@ -163,12 +182,12 @@ module.exports = function(grunt) {
 		grunt.log.oklns('2) Written app/index.php which tells wordpress that it is in a subdirectory');
 		grunt.log.oklns('********************************************');
 	});
-	
+
 	// Compile styles, and watch for changes
 	grunt.registerTask('style', ['compass:dev', 'autoprefixer:dev']);
-	
+
 	grunt.registerTask('watcher', ['watch']);
-	
+
 	// Dynamically add necessary paths to wp-config.php. You will need to edit this.
 
 	grunt.registerTask('production', function() {
