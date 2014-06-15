daf81bd1183dfbb4f42be98c1120837e6d9a88eb
Dave Bowker <dave@davebowker.com>
2014-06-15 22:19:35 +0100

    working on ssh

--- config/sftp.js
+++ config/sftp.js
@@ -1,12 +1,16 @@
 'use strict';
 
 module.exports = {
-	uptime: {
-		command: 'uptime',
+	publish: {
+		files: {
+			"./": "app/**"
+		},
 		options: {
 			host: '<%= secret.host %>',
 			username: '<%= secret.username %>',
 			password: '<%= secret.password %>',
+			path: '<%= secret.www_dir %>',
+			srcBasePath: "app/",
 			showProgress: true
 		}
 	}
---
+++ config/shell.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+	npm : {
+		command : 'npm update'
+	}
+};
